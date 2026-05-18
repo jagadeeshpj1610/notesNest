dabe967490186f7e38aa50f28f49d8bcf59e959b
Jagadeesh <22022cm040@gmail.com>
2026-05-18 13:58:03 +0530

    #PJ add the login function to login into their account

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -39,4 +39,39 @@ const register = async (req, res) => {
     }
 }
 
-module.exports = {register}
+const login = async (req, res) => {
+    try {
+        const {email, password} = req.body
+        if (!email || !password) {
+            return res.status(400).json({message : "all fileds are required!"})
+        }
+        const user = await User.findOne({email})
+        if (!user) {
+           return res.status(400).json({message: "user not found"}) 
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password)
+        if (!isPasswordValid) {
+            return res.status(401).json({message : "invalid credentials"})
+        }
+        const token = jwt.sign(
+            {id:user._id, role:user.role},
+            process.env.JWT_SECRET,
+            {expiresIn:'7d'}
+        )
+        res.status(200).json({
+            message:"Login sucessfull",
+            token,
+            user:{
+                id:user._id,
+                name:user.name,
+                email: user.email,
+                role: user.role,
+            }
+        })
+    } catch (error) {
+        console.log("Login error", error);
+        res.status(500).json({message:"server error"})
+    }
+}
+
+module.exports = {register, login}
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const {register} = require("../controllers/authController")
+const {register, login} = require("../controllers/authController")
+const { route } = require('../server')
 
 router.post("/register", register)
+router.post("/login", login)
 
 module.exports = router
