8e7e33ac58f9d685a62b596ed8fa24fdf23d7d0f
Jagadeesh <22022cm040@gmail.com>
2026-05-18 16:28:59 +0530

    #PJ #add the forgot password and reset password routes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const {register, login, logout, refresh, verifyOTP} = require("../controllers/authController")
+const {register, login, logout, refresh, verifyOTP, forgotPassword, resetPassword} = require("../controllers/authController")
 
 const { protect } = require("../middleware/authMiddleware")
 
@@ -9,5 +9,7 @@ router.post("/login", login)
 router.post('/logout', protect, logout)
 router.post('/refresh', refresh)
 router.post('/verify-otp', verifyOTP)
+router.post('/forgot-password', forgotPassword)
+router.post('/reset-password', resetPassword)
 
 module.exports = router
