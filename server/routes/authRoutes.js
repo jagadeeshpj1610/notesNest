6dba354fb5da04553ffca046dba9ed5da4ad9b16
Jagadeesh <22022cm040@gmail.com>
2026-05-18 14:13:47 +0530

    #PJ add the middleware for protection

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,9 +2,10 @@ const express = require('express')
 const router = express.Router()
 const {register, login, logout} = require("../controllers/authController")
 const { route } = require('../server')
+const { protect } = require("../middleware/authMiddleware")
 
 router.post("/register", register)
 router.post("/login", login)
-router.post('/logout', logout)
+router.post('/logout', protect, logout)
 
 module.exports = router
